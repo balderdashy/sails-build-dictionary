f39a1e9f987708fcf58f004b1c5b7d0588a671de
Mike McNeil <mike@balderdash.co>
2016-08-16 03:18:54 -0500

    Updated editorconfig,jshintrc, and gitignore, and applied code conventions.

--- index.js
+++ index.js
@@ -2,137 +2,141 @@
  * Module dependencies
  */
 
-var _			= require('lodash'),
-	includeAll	= require('include-all');
+var _ = require('lodash');
+var includeAll = require('include-all');
 
 
 
 /**
  * buildDictionary
- * 
+ *
  * Go through each object, include the code, and determine its identity.
  * Tolerates non-existent files/directories by ignoring them.
 
-	@param {Object} options {
-		dirname			:: the path to the source directory
+  @param {Object} options {
+    dirname     :: the path to the source directory
 
-		identity		:: If disabled, (explicitly set to false) don't inject an identity into the module
-						   also don't try to use the bundled `identity` property in the module to determine
-						   the keyname in the result object
-						   default: true
+    identity    :: If disabled, (explicitly set to false) don't inject an identity into the module
+               also don't try to use the bundled `identity` property in the module to determine
+               the keyname in the result object
+               default: true
 
-		optional		:: if enabled, fail silently and return {} when source directory does not exist
-						   or cannot be read (otherwise, exit w/ an error)
-						   default: false
+    optional    :: if enabled, fail silently and return {} when source directory does not exist
+               or cannot be read (otherwise, exit w/ an error)
+               default: false
 
-		depth			:: the level of recursion where modules will be included
-						   defaults to infinity? (not sure)
+    depth     :: the level of recursion where modules will be included
+               defaults to infinity? (not sure)
 
-		filter			:: only include modules whose FILENAME matches this regex
-						   default `undefined`
+    filter      :: only include modules whose FILENAME matches this regex
+               default `undefined`
 
-		pathFilter		:: only include modules whose FULL RELATIVE PATH matches this regex
-						   (relative from the entry point directory)
-						   default `undefined`
+    pathFilter    :: only include modules whose FULL RELATIVE PATH matches this regex
+               (relative from the entry point directory)
+               default `undefined`
 
-		replaceExpr		:: in identity: use this regex to remove substrings like 'Controller' or 'Service'
-						   and replace them with the value of `replaceVal`
+    replaceExpr   :: in identity: use this regex to remove substrings like 'Controller' or 'Service'
+               and replace them with the value of `replaceVal`
 
-		replaceVal		:: see above
-						   default value: ''
+    replaceVal    :: see above
+               default value: ''
 
-		dontLoad		:: if `dontLoad` is set to true, don't run the module w/ V8 or load it into memory-- 
-						   instead, return a tree representing the directory structure
-						   (all extant file leaves are included as keys, with their value = `true`)
+    dontLoad    :: if `dontLoad` is set to true, don't run the module w/ V8 or load it into memory--
+               instead, return a tree representing the directory structure
+               (all extant file leaves are included as keys, with their value = `true`)
 
-		useGlobalIdForKeyName
-						:: if `useGlobalIdForKeyName` is set to true, don't lowercase the identity to get
-						   the keyname-- just use the globalId.
-	}
+    useGlobalIdForKeyName
+            :: if `useGlobalIdForKeyName` is set to true, don't lowercase the identity to get
+               the keyname-- just use the globalId.
+  }
 
-	@param {Function} cb
+  @param {Function} cb
  */
 
 module.exports = buildDictionary;
 
 function buildDictionary (options, cb) {
 
-	// Defaults
-	options.replaceVal = options.replaceVal || '';
+  // Defaults
+  options.replaceVal = options.replaceVal || '';
 
-	// Deliberately exclude source control directories
-	if (!options.excludeDirs) {
-		options.excludeDirs = /^\.(git|svn)$/;
-	}
+  // Deliberately exclude source control directories
+  if (!options.excludeDirs) {
+    options.excludeDirs = /^\.(git|svn)$/;
+  }
 
-	var files = includeAll(options);
+  var files = includeAll(options);
 
-	// Start building the module dictionary
-	var dictionary = {};
+  // Start building the module dictionary
+  var dictionary = {};
 
-	// Iterate through each module in the set
-	_.each(files, function(module, filename) {
+  // Iterate through each module in the set
+  _.each(files, function(module, filename) {
 
-		// Build the result object by merging all of the target modules
-		// Note: Each module must export an object in order for this to work
-		// (e.g. for building a configuration object from a set of config files)
-		if (options.aggregate) {
+    // Build the result object by merging all of the target modules
+    // Note: Each module must export an object in order for this to work
+    // (e.g. for building a configuration object from a set of config files)
+    if (options.aggregate) {
 
-			// Check that source module is a valid object
-			if ( !_.isPlainObject(module) ) {
-				return cb(new Error('Invalid module:' + module));
-			}
+      // Check that source module is a valid object
+      if (!_.isPlainObject(module)) {
+        return cb(new Error('Invalid module:' + module));
+      }
 
-			// Merge module into dictionary
-			_.merge(dictionary, module);
+      // Merge module into dictionary
+      _.merge(dictionary, module);
 
-			return;
-		}
+      return;
+    }
 
-		// Keyname is how the module will be identified in the returned module tree
-		var keyName = filename;
+    // Keyname is how the module will be identified in the returned module tree
+    var keyName = filename;
 
-		// If a module is found but marked as `undefined`,
-		// don't actually include it (since it's probably unusable)
-		if (typeof module === 'undefined') {
-			return;
-		}
+    // If a module is found but marked as `undefined`,
+    // don't actually include it (since it's probably unusable)
+    if (typeof module === 'undefined') {
+      return;
+    }
 
-		// Unless the `identity` option is explicitly disabled,
-		// (or `dontLoad` is set)
-		if (!options.dontLoad && options.identity !== false) {
+    // Unless the `identity` option is explicitly disabled,
+    // (or `dontLoad` is set)
+    if (!options.dontLoad && options.identity !== false) {
 
-			// If no `identity` property is specified in module, infer it from the filename
-			if (!module.identity) {
-				if (options.replaceExpr) {
-					module.identity = filename.replace(options.replaceExpr, options.replaceVal);
-				}
-				else module.identity = filename;
-			}
+      // If no `identity` property is specified in module, infer it from the filename
+      if (!module.identity) {
+        if (options.replaceExpr) {
+          module.identity = filename.replace(options.replaceExpr, options.replaceVal);
+        }
+        else {
+          module.identity = filename;
+        }
+      }
 
-			// globalId is the name of the variable for this module 
-			// that will be exposed globally in Sails unless configured otherwise
-			
-			// Generate `globalId` using the original value of module.identity
-			if (!module.globalId) {module.globalId = module.identity;}
+      // globalId is the name of the variable for this module
+      // that will be exposed globally in Sails unless configured otherwise
 
-			// `identity` is the all-lowercase version
-			module.identity = module.identity.toLowerCase();
+      // Generate `globalId` using the original value of module.identity
+      if (!module.globalId) {module.globalId = module.identity;}
 
-			// Use the identity for the key name
-			keyName = options.useGlobalIdForKeyName ? module.globalId : module.identity;
-		}
+      // `identity` is the all-lowercase version
+      module.identity = module.identity.toLowerCase();
 
-		// Save the module's contents in our dictionary object
-		// (this will actually just be `true` if the `dontLoad` option is set)
-		dictionary[keyName] = module;
-	});
+      // Use the identity for the key name
+      keyName = options.useGlobalIdForKeyName ? module.globalId : module.identity;
+    }
 
-	// Always return at least an empty object
-	dictionary = dictionary || {};
-	
-	return cb(null, dictionary);
-};
+    // Save the module's contents in our dictionary object
+    // (this will actually just be `true` if the `dontLoad` option is set)
+    dictionary[keyName] = module;
+  });
+
+  // Always return at least an empty object
+  dictionary = dictionary || {};
+
+  return cb(null, dictionary);
+}
+
+
 
 /**
  * Build a dictionary of named modules
@@ -143,8 +147,9 @@ function buildDictionary (options, cb) {
  */
 
 module.exports.required = function(options, cb) {
-	return buildDictionary(options, cb);
-},
+  return buildDictionary(options, cb);
+};
+
 
 
 /**
@@ -156,9 +161,10 @@ module.exports.required = function(options, cb) {
  */
 
 module.exports.optional = function(options, cb) {
-	options.optional = true;
-	return buildDictionary(options, cb);
-},
+  options.optional = true;
+  return buildDictionary(options, cb);
+};
+
 
 
 /**
@@ -170,10 +176,11 @@ module.exports.optional = function(options, cb) {
  */
 
 module.exports.exists = function(options, cb) {
-	options.optional = true;
-	options.dontLoad = false;
-	return buildDictionary(options, cb);
-},
+  options.optional = true;
+  options.dontLoad = false;
+  return buildDictionary(options, cb);
+};
+
 
 
 /**
@@ -185,7 +192,7 @@ module.exports.exists = function(options, cb) {
  */
 
 module.exports.aggregate = function(options, cb) {
-	options.aggregate = true;
-	options.optional = true;
-	return buildDictionary(options, cb);
-}
+  options.aggregate = true;
+  options.optional = true;
+  return buildDictionary(options, cb);
+};
